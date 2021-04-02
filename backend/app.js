945daa4e9eae2acce3bf707d8ecba0fad6e6470c
Brice Tchijouk <63953094+Brissok@users.noreply.github.com>
2021-04-02 17:53:58 +0200

    structure back-end (routes, controllers)

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const User = require('./models/User');
-const Sauce = require('./models/Sauce');
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://brissok:WB2TW0R8q5j8fQyJ@cluster0pekocko.r10ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://brissok:WB2TW0R8q5j8fQyJ@cluster0pekocko.r10ly.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -22,69 +22,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-//POST signup - Chiffre le mot de passe de l'utilisateur, ajoute l'utilisateur à la base de données
-app.post('/api/auth/signup', (req, res, next) => {
-  delete req.body._id;
-  const user = new User({
-    ...req.body
-  });
-  user.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//POST login - vérifie les infos d'id user, en renvoyant userID depuis la BDD et un jeton Web JSON signé (contenant userID)
-app.post('/api/auth/login', (req, res, next) => {
-  delete req.body._id;
-  const user = new User({
-    ...req.body
-  });
-  user.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//GET Tableau des sauces - Renvoie le tableau de toutes les sauces dans la BDD
-app.get('/api/sauces', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-});
-
-//GET sauce unique - Renvoie la sauce avec l'ID fourni
-app.get('/api/sauces/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauces => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
-});
-
-//POST sauce - Capture et enregistre l'image, analyse la sauce en utilisant une chaîne de caractères et l'enregistre dans la base de données, en définissant correctement son image URL. Remet les sauces aimées et celles détestées à 0, et les sauces usersliked et celles usersdisliked aux tableaux vides.
-app.post('/api/sauces', (req, res, next) => {
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-//PUT sauce - Màj la sauce avec l'ID fourni
-app.put('api/sauces/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-//DELETE sauce - supprime la sauce avec l'ID fourni
-app.delete('api/sauces/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-    .catch(error => res.status(400).json({ error }));
-})
-
-//POST like
-
-
-
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
